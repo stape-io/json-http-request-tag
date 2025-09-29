0f28e7bee041fdbad6884d013432ae7ebafdb15b
giovaniortolani <9199996+giovaniortolani@users.noreply.github.com>
2025-09-29 14:58:18 -0300

    Preserve nested fields in body

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# JSON HTTP Request Tag for Google Tag Manager Server Side 
+# JSON HTTP Request Tag for Google Tag Manager Server Side
 
-JSON HTTP Request - Custom Tag Template (Server) for Google Tag Manager.
+The **JSON HTTP Request Tag** is a custom tag for the Google Tag Manager server container that allows you to send event data to any third-party endpoint via an HTTP request with a JSON payload.
 
+This tag is highly flexible, enabling you to forward data from your server-side GTM to CRMs, analytics platforms, or any other service that accepts JSON data.
 
-How to use it and more info can be found in [this article](https://stape.io/how-to-send-all-ga-event-data-to-your-url-site-crm/).
-
+## Useful Resources
+- How to use it and more info can be found in [this article](https://stape.io/how-to-send-all-ga-event-data-to-your-url-site-crm/).
 
 ## Open Source
 
-JSON HTTP Request Tag for GTM Server Side is developed and maintained by [Stape Team](https://stape.io/) under the Apache 2.0 license.
+The **JSON HTTP Request Tag for GTM Server Side** is developed and maintained by the [Stape Team](https://stape.io/) under the Apache 2.0 license.
--- template.js
+++ template.js
@@ -1,16 +1,24 @@
-﻿const sendHttpRequest = require('sendHttpRequest');
+﻿const BigQuery = require('BigQuery');
 const getAllEventData = require('getAllEventData');
+const getContainerVersion = require('getContainerVersion');
+const getRequestHeader = require('getRequestHeader');
+const getTimestampMillis = require('getTimestampMillis');
+const getType = require('getType');
+const JSON = require('JSON');
+const logToConsole = require('logToConsole');
 const makeInteger = require('makeInteger');
 const makeTableMap = require('makeTableMap');
-const JSON = require('JSON');
-const getRequestHeader = require('getRequestHeader');
-const logToConsole = require('logToConsole');
-const getContainerVersion = require('getContainerVersion');
+const sendHttpRequest = require('sendHttpRequest');
 
-const containerVersion = getContainerVersion();
-const isDebug = containerVersion.debugMode;
-const isLoggingEnabled = determinateIsLoggingEnabled();
-const traceId = getRequestHeader('trace-id');
+/*==============================================================================
+==============================================================================*/
+
+const eventData = getAllEventData();
+
+const url = getUrl(eventData);
+if (url && url.lastIndexOf('https://gtm-msr.appspot.com/', 0) === 0) {
+  return data.gtmOnSuccess();
+}
 
 const postHeaders = { 'Content-Type': 'application/json' };
 let postBodyData = {};
@@ -26,7 +34,7 @@ if (data.headers) {
 }
 
 if (data.data) {
-  let postBodyCustomData = data.simple_object ? createSimpleObject() : createNestedObject();
+  const postBodyCustomData = data.simple_object ? createSimpleObject() : createNestedObject();
 
   for (let key in postBodyCustomData) {
     postBodyData[key] = postBodyCustomData[key];
@@ -42,46 +50,34 @@ if (data.inside_array) {
 }
 
 const postBody = JSON.stringify(postBodyData);
-let requestOptions = { headers: postHeaders, method: data.requestMethod };
+const requestOptions = { headers: postHeaders, method: data.requestMethod };
 
 if (data.requestTimeout) {
   requestOptions.timeout = makeInteger(data.requestTimeout);
 }
 
-if (isLoggingEnabled) {
-  logToConsole(
-    JSON.stringify({
-      Name: 'JsonRequest',
-      Type: 'Request',
-      TraceId: traceId,
-      RequestMethod: data.requestMethod,
-      RequestUrl: data.url,
-      RequestBody: postBodyData
-    })
-  );
-}
+log({
+  Name: 'JsonRequest',
+  Type: 'Request',
+  RequestMethod: data.requestMethod,
+  RequestUrl: data.url,
+  RequestBody: postBodyData
+});
 
 sendHttpRequest(
   data.url,
   (statusCode, headers, body) => {
-    if (isLoggingEnabled) {
-      logToConsole(
-        JSON.stringify({
-          Name: 'JsonRequest',
-          Type: 'Response',
-          TraceId: traceId,
-          ResponseStatusCode: statusCode,
-          ResponseHeaders: headers,
-          ResponseBody: body
-        })
-      );
-    }
+    log({
+      Name: 'JsonRequest',
+      Type: 'Response',
+      ResponseStatusCode: statusCode,
+      ResponseHeaders: headers,
+      ResponseBody: body
+    });
+
     if (!data.useOptimisticScenario) {
-      if (statusCode >= 200 && statusCode < 300) {
-        data.gtmOnSuccess();
-      } else {
-        data.gtmOnFailure();
-      }
+      if (statusCode >= 200 && statusCode < 300) data.gtmOnSuccess();
+      else data.gtmOnFailure();
     }
   },
   requestOptions,
@@ -92,12 +88,15 @@ if (data.useOptimisticScenario) {
   return data.gtmOnSuccess();
 }
 
+/*==============================================================================
+  Vendor related functions
+==============================================================================*/
+
 function escapeKeys(ob) {
-  var toReturn = {};
+  const toReturn = {};
 
   for (let key in ob) {
-    let newKey = key.split('-').join('_');
-
+    const newKey = key.split('-').join('_');
     toReturn[newKey] = ob[key];
   }
 
@@ -105,22 +104,22 @@ function escapeKeys(ob) {
 }
 
 function flatten(ob) {
-  var toReturn = {};
+  const toReturn = {};
 
   for (let i in ob) {
     if (!ob.hasOwnProperty(i)) continue;
 
-    if (typeof ob[i] == 'object') {
-      var flatObject = flatten(ob[i]);
-      for (var x in flatObject) {
+    if (getType(ob[i]) === 'object') {
+      const flatObject = flatten(ob[i]);
+      for (let x in flatObject) {
         if (!flatObject.hasOwnProperty(x)) continue;
-
         toReturn[i + '_' + x] = flatObject[x];
       }
     } else {
       toReturn[i] = ob[i];
     }
   }
+
   return toReturn;
 }
 
@@ -128,60 +127,98 @@ function createSimpleObject() {
   return makeTableMap(data.data, 'key', 'value');
 }
 
-function deepMerge(target, source) {
-  if (!target || typeof target !== 'object') {
-    target = {};
-  }
-  for (var key in source) {
-    if (!source.hasOwnProperty(key)) continue;
-
-    var sv = source[key];
-    var tv = target[key];
-
-    if (sv && typeof sv === 'object') {
-      // no Array check, assume it's always an object-like
-      target[key] = deepMerge(tv && typeof tv === 'object' ? tv : {}, sv);
-    } else {
-      target[key] = sv;
-    }
-  }
-  return target;
-}
-
 function createNestedObject() {
-  let object = {};
+  const result = {};
 
-  for (let key in data.data) {
-    let dotPath = data.data[key].key;
-    let rootProperty = dotPath.split('.')[0];
-    let strObj = strToObj(dotPath, data.data[key].value)[rootProperty];
+  data.data.forEach((item) => {
+    const path = item.key.split('.');
+    path.reduce((acc, key, index) => {
+      const isLastKey = index === path.length - 1;
+      if (isLastKey) acc[key] = item.value;
+      else acc[key] = acc[key] || {};
+      return acc[key];
+    }, result);
+  });
 
-    if (object[rootProperty]) {
-      object[rootProperty] = deepMerge(object[rootProperty], strObj);
-    } else {
-      object[rootProperty] = strObj;
-    }
-  }
-
-  return object;
+  return result;
 }
 
-function strToObj(dotPath, val) {
-  let i,
-    obj = {},
-    dotArr = dotPath.split('.');
-  let x = obj;
+/*==============================================================================
+  Helpers
+==============================================================================*/
 
-  for (i = 0; i < dotArr.length - 1; i++) {
-    x = x[dotArr[i]] = {};
+function getUrl(eventData) {
+  return eventData.page_location || eventData.page_referrer || getRequestHeader('referer');
+}
+
+function log(rawDataToLog) {
+  const logDestinationsHandlers = {};
+  if (determinateIsLoggingEnabled()) logDestinationsHandlers.console = logConsole;
+  if (determinateIsLoggingEnabledForBigQuery()) logDestinationsHandlers.bigQuery = logToBigQuery;
+
+  rawDataToLog.TraceId = getRequestHeader('trace-id');
+
+  const keyMappings = {
+    // No transformation for Console is needed.
+    bigQuery: {
+      Name: 'tag_name',
+      Type: 'type',
+      TraceId: 'trace_id',
+      EventName: 'event_name',
+      RequestMethod: 'request_method',
+      RequestUrl: 'request_url',
+      RequestBody: 'request_body',
+      ResponseStatusCode: 'response_status_code',
+      ResponseHeaders: 'response_headers',
+      ResponseBody: 'response_body'
+    }
+  };
+
+  for (const logDestination in logDestinationsHandlers) {
+    const handler = logDestinationsHandlers[logDestination];
+    if (!handler) continue;
+
+    const mapping = keyMappings[logDestination];
+    const dataToLog = mapping ? {} : rawDataToLog;
+
+    if (mapping) {
+      for (const key in rawDataToLog) {
+        const mappedKey = mapping[key] || key;
+        dataToLog[mappedKey] = rawDataToLog[key];
+      }
+    }
+
+    handler(dataToLog);
   }
+}
 
-  x[dotArr[i]] = val;
+function logConsole(dataToLog) {
+  logToConsole(JSON.stringify(dataToLog));
+}
 
-  return obj;
+function logToBigQuery(dataToLog) {
+  const connectionInfo = {
+    projectId: data.logBigQueryProjectId,
+    datasetId: data.logBigQueryDatasetId,
+    tableId: data.logBigQueryTableId
+  };
+
+  dataToLog.timestamp = getTimestampMillis();
+
+  ['request_body', 'response_headers', 'response_body'].forEach((p) => {
+    dataToLog[p] = JSON.stringify(dataToLog[p]);
+  });
+
+  BigQuery.insert(connectionInfo, [dataToLog], { ignoreUnknownValues: true });
 }
 
 function determinateIsLoggingEnabled() {
+  const containerVersion = getContainerVersion();
+  const isDebug = !!(
+    containerVersion &&
+    (containerVersion.debugMode || containerVersion.previewMode)
+  );
+
   if (!data.logType) {
     return isDebug;
   }
@@ -195,4 +232,9 @@ function determinateIsLoggingEnabled() {
   }
 
   return data.logType === 'always';
+}
+
+function determinateIsLoggingEnabledForBigQuery() {
+  if (data.bigQueryLogType === 'no') return false;
+  return data.bigQueryLogType === 'always';
 }
